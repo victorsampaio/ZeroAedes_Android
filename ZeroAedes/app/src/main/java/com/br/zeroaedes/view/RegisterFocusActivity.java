858883616865b0e6a4e0e56bc624642c4e53224b
Victor Fernando de Oliveira Sampaio <victorfsampaio@gmail.com>
2016-04-07 02:20:47 -0300

    ZeroAedes - v 0.0.2
    New activity created - RegisterFocusActivity

---
+++ ZeroAedes/app/src/main/java/com/br/zeroaedes/view/RegisterFocusActivity.java
@@ -0,0 +1,15 @@
+package com.br.zeroaedes.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.br.zeroaedes.R;
+
+public class RegisterFocusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_focus);
+    }
+}
